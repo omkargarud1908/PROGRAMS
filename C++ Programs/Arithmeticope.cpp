bb3fb76ce0cbadca39e9418f595de1d9ccab42fe
OMKAR <124242932+omkargarud1908@users.noreply.github.com>
2023-02-07 23:26:58 +0530

    OG1908

--- C++ Programs/Arithmeticope.cpp
+++ C++ Programs/Arithmeticope.cpp
@@ -5,23 +5,28 @@ Write a C++ program to read two float numbers. Perform arithmetic operations lik
 #include <iostream>
 using namespace std;
 
-inline float add(float a, float b) {
+inline float add(float a, float b) 
+{
     return a + b;
 }
 
-inline float subs(float a, float b) {
+inline float subs(float a, float b) 
+{
     return a - b;
 }
 
-inline float mul(float a, float b) {
+inline float mul(float a, float b) 
+{
     return a * b;
 }
 
-inline float div(float a, float b) {
+inline float div(float a, float b) 
+{
     return a / b;
 }
 
-int main() {
+int main() 
+{
     float f1, f2;
 
     cout << "Enter num1: ";
