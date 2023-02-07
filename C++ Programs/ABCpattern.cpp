1a07fecd508feae1e4658ee0aa7f695b3915cfcc
OMKAR <124242932+omkargarud1908@users.noreply.github.com>
2023-02-07 23:23:09 +0530

    OG1908

--- C++ Programs/ABCpattern.cpp
+++ C++ Programs/ABCpattern.cpp
@@ -13,9 +13,11 @@ int main()
 {
     int  k =1;
 
-    for(int i = 1; i <= 4; i++) {
+    for(int i = 1; i <= 4; i++) 
+    {
 
-        for(int j = 1; j <= i; j++, k++) {
+        for(int j = 1; j <= i; j++, k++) 
+        {
             cout << (char)(k + 64);
         }
 
